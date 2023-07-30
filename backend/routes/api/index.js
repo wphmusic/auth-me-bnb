d833551934c5bf821f6fbd531ad02c9ed3d9b4f4
Kenneth Fisher <kenandcrys@live.com>
2023-07-30 13:14:36 -0500

    Phase 4 part 1

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 const { setTokenCookie } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
 const { restoreUser } = require('../../utils/auth.js');
@@ -12,10 +14,6 @@ router.get(
     return res.json(req.user);
   }
 );
-
-router.post('/test', function(req, res) {
-    res.json({ requestBody: req.body });
-  });
 
 // GET /api/set-token-cookie
 router.get('/set-token-cookie', async (_req, res) => {
@@ -36,5 +34,13 @@ router.get(
     return res.json(req.user);
   }
 );
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+router.post('/test', (req, res) => {
+  res.json({ requestBody: req.body });
+});
 
 module.exports = router;
